22958bb4082de80ea8a0207146c1fa8420abad8d
Erik Lorenz <erik.e.lorenz@gmail.com>
2014-12-26 18:08:51 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Clock100
+
+## Description
+
+A client-side, cached (offline) web page, which displays the current time, but with 10 hours in a day, 100 minutes in an hour and 100 seconds in a minute.
+
+Each "second" is only 0.864 seconds long.
+
+## Usage / Installation
+
+Open with your web browser.
---
+++ main.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+  var body;
+
+  function getLarsTime(date){
+    date = date || new Date();
+
+    return (date.getSeconds() + date.getMilliseconds()/1000 + date.getMinutes()*60 + date.getHours()*3600) / 0.864;
+  }
+
+  function formatLarsTime(larstime){
+    larstime = larstime || getLarsTime();
+
+    larstime = Math.floor(larstime);
+    while (larstime.length < 5) {
+      larstime = '0' + larstime;
+    }
+
+    return larstime.toString().replace(/(.)(..)(..)/, '$1:$2:$3');
+  }
+
+  function updateLarsTime(){
+    body.innerHTML = formatLarsTime();
+  }
+
+  body = document.getElementsByTagName('body')[0];
+
+  setInterval(updateLarsTime, 100);
+};
